a699cbfcdd042861490c06420de663aa7d3945ec
Jana Nekvapilová <jana.nekva@gmail.com>
2025-09-15 19:59:39 +0200

    extend RoomsPage with data fetching and loading state

--- src/Components/RoomsPage/RoomsPage.jsx
+++ src/Components/RoomsPage/RoomsPage.jsx
@@ -1,11 +1,37 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import { RoomDetail } from "../RoomDetail/RoomDetail"
 import { RoomsList } from "../RoomsList/RoomsList"
 
 export const RoomsPage = () => {
+
+    const [rooms, setRooms] = useState([]);
+
+    useEffect(() => {
+         const fetchRooms = async () => {
+                const response = await fetch('http://localhost:4000/api/rooms')  
+                const json = await response.json();
+                setRooms(json.data);
+                console.log(json.data)
+         }  
+
+         fetchRooms();
+        
+    }, []);
+
+
     return (
-        <>
-        < RoomsList />
-        < RoomDetail />
-        </>
+<>
+        {rooms.length === 0
+                ? 'Načítám data...'
+                : (
+                    <>
+                        < RoomsList />
+                        < RoomDetail />
+                    </>
+                )
+        }
+
+</>
     )
 }
