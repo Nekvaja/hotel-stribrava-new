70e4d2c54cd7eb8036fd72480e6fafcde8b1329a
Jana Nekvapilová <jana.nekva@gmail.com>
2025-09-20 13:40:24 +0200

    Update form structure and labels

--- src/Components/Formular/Formular.jsx
+++ src/Components/Formular/Formular.jsx
@@ -4,22 +4,43 @@ export const Formular = () => {
     return (
           <form>
               <div className="form-fields">
-                <label htmlFor="field1" className="field-label">Field 1:</label>
+                <label htmlFor="field1" className="field-label">Od:</label>
                 <input id="field1" className="field-input" type="text" />
 
-                <label htmlFor="field2" className="field-label">Field 2:</label>
+                <label htmlFor="field2" className="field-label">Do:</label>
                 <input id="field2" className="field-input" type="text" />
-
-                <label htmlFor="select" className="field-label">Select:</label>
+                
+                <label htmlFor="select1" className="field-label">Počet osob:</label>
                 <select id="select" className="field-input">
-                  <option>Option 1</option>
-                  <option>Option 2</option>
-                  <option>Option 3</option>
-                  <option>Option 4</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </select>
+                
+                <label htmlFor="select2" className="field-label">Stravování:</label>
+                <select id="select" className="field-input">
+                  <option>Snídaně</option>
+                  <option>Polopenze</option>
+                  <option>Plná penze</option>
                 </select>
 
-                <label htmlFor="check1" className="field-label">Checkbox 1:</label>
+                <label htmlFor="check1" className="field-label">Domácí mazlíček:</label>
                 <input id="check1" className="field-input" type="checkbox" />
+                
+                <label htmlFor="check2" className="field-label">Přistýlka pro dítě:</label>
+                <input id="check1" className="field-input" type="checkbox" />
+                
+                <label htmlFor="check3" className="field-label">Bezbariérový přístup:</label>
+                <input id="check1" className="field-input" type="checkbox" />
+
+                <label htmlFor="field4" className="field-label">email:</label>
+                <input id="field1" className="field-input" type="text" />
+
+                <label htmlFor="field5" className="field-label">Telefon:</label>
+                <input id="field1" className="field-input" type="text" />
+
               </div>
               <button className="wide">Submit</button>
             </form>
