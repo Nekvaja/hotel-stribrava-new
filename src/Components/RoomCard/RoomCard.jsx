f3f77fd4d301ad888891483717d7c44f7cd35fd5
Jana Nekvapilová <jana.nekva@gmail.com>
2025-09-20 11:22:28 +0200

    Update RoomCard body: show room.price instead of room.description

--- src/Components/RoomCard/RoomCard.jsx
+++ src/Components/RoomCard/RoomCard.jsx
@@ -6,7 +6,7 @@ export const RoomCard = ({ room }) => {
       <div className="card">
         <img className="card__image" src={room.image} />
         <div className="card__title">{room.name}</div>
-        <div className="card__body">{room.description}</div>
+        <div className="card__body">{room.price} Kč na osobu</div>
       </div>
   
   )
